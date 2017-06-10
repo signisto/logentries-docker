a819a359b7e01b2076dd67adfbdedbfcf707095f
Marc Qualie <marc@marcqualie.com>
2017-06-10 22:22:26 +0100

    Add LE_DESTINATION variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 sed -i "s|\${LE_NAME}|$LE_NAME|g" /etc/le/config
 sed -i "s|\${LE_TOKEN}|$LE_TOKEN|g" /etc/le/config
 sed -i "s|\${LE_PATH}|$LE_PATH|g" /etc/le/config
+sed -i "s|\${LE_DESTINATION}|$LE_DESTINATION|g" /etc/le/config
 
 cat /etc/le/config
 
